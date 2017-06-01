3884f14825f853e12ddb615468e1546dabe7d2a9
user1990 <paulius.rimg1990@gmail.com>
2017-06-01 13:26:26 +0300

    Removed some rules

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 
+// Initialize app
 const app = express();
 
-app.listen(3000, (err) => {
+// Middleware for HTTP request logs
+app.use(morgan('dev'));
+
+// Home route
+app.get('/', (req, res) => {
+  res.json('Home Page');
+});
+
+// Listen to port
+app.listen(3000, err => {
   if (err) { throw err; }
   console.log('==============================');
   console.log('Server is running on port 4000');
---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+
+/* The user schema attributes/characteristics/fields */
+let UserSchema = new mongoose.Schema({
+  email: {type: String, unique: true, lowercase: true},
+  password: String,
+
+  profile: {
+    name: {type: String, default: ''},
+    picture: {type: String, default: ''}
+  },
+
+  address: String,
+  history: [{
+    date: Date,
+    paid: {type: Number, default: 0}
+  }]
+});
+
+/* Hash the password before we even save it to the database */
+UserSchema.pre('save', next => {
+  let user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+/* Compare password in the database and one typed be the user */
+UserSchema.methods.comparePassword = (password) => {
+  return bcrypt.compareSync(password, this.password);
+};
+
